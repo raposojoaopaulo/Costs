53edfd85b81f892edd472eb03429bd087da5439c
raposojoaopaulo <raposojoaopaulo@gmail.com>
2022-04-19 17:48:02 -0300

    adding db.json and config inputs/forms

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,14 +1,38 @@
-import ProjectForm from "../project/ProjecForm"
+import { useNavigate } from "react-router-dom"
+import ProjectForm from "../project/ProjectForm"
 
 import styles from "./NewProject.module.css"
 
 function NewProject() {
-  
+  const history = useNavigate()
+
+  function createPost(project){
+
+    // initialize cost and services
+    project.cost = 0
+    project.services = []
+
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) =>{
+        console.log(data)
+        //redirect
+        history('/projects', { message: 'Project created successfully!' })
+      })
+      .cath((err) => console.log(err))
+  }
+
   return (
     <div className={styles.newproject_container} >
       <h1>Create Project</h1>
       <p>Create your project and then add the services</p>
-      <ProjectForm />
+      <ProjectForm handleSubmit={createPost} btnText="Create Project"/>
     </div>
   )
 
